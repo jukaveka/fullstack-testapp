a63698d6fad29a43d45ea4504564f73493667e9b
Juho Kavekari <juhokavekari@hotmail.com>
2025-03-20 18:38:33 +0200

    Remove obsolete middleware cors

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
 
